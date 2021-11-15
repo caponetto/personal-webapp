703f452877b4c38c0c9edbd0c7e5bb15c0e7074b
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2021-11-15 16:11:01 -0300

    Add test: NavigationSmallScreen

---
+++ cypress/integration/NavigationSmallScreenTest.ts
@@ -0,0 +1,68 @@
+describe("NavigationSmallScreen", () => {
+  beforeEach(() => {
+    cy.viewport(600, 800);
+  });
+
+  it("should navigate to /about when clicking on the FaceBadge from other route", () => {
+    cy.visit("/text");
+    cy.get("#open-drawer-button").click();
+    cy.get("#temporary-drawer").within(() => {
+      cy.get("#face-badge").click();
+    });
+    cy.url().should("include", "/about");
+  });
+
+  it("should continue in /about when clicking on the FaceBadge from /about", () => {
+    cy.visit("/about");
+    cy.get("#open-drawer-button").click();
+    cy.get("#temporary-drawer").within(() => {
+      cy.get("#face-badge").click();
+    });
+    cy.url().should("include", "/about");
+  });
+
+  it("should navigate to /about when clicking in the About list item", () => {
+    cy.visit("/text");
+    cy.get("#open-drawer-button").click();
+    cy.get("#temporary-drawer").within(() => {
+      cy.get("#About-item-button").click();
+    });
+    cy.url().should("include", "/about");
+  });
+
+  it("should navigate to /journey when clicking in the Journey list item", () => {
+    cy.visit("/");
+    cy.get("#open-drawer-button").click();
+    cy.get("#temporary-drawer").within(() => {
+      cy.get("#Journey-item-button").click();
+    });
+    cy.url().should("include", "/journey");
+  });
+
+  it("should navigate to /text when clicking in the Text list item", () => {
+    cy.visit("/");
+    cy.get("#open-drawer-button").click();
+    cy.get("#temporary-drawer").within(() => {
+      cy.get("#Text-item-button").click();
+    });
+    cy.url().should("include", "/text");
+  });
+
+  it("should navigate to /talk when clicking in the Talk list item", () => {
+    cy.visit("/");
+    cy.get("#open-drawer-button").click();
+    cy.get("#temporary-drawer").within(() => {
+      cy.get("#Talk-item-button").click();
+    });
+    cy.url().should("include", "/talk");
+  });
+
+  it("should navigate to /code when clicking in the Code list item", () => {
+    cy.visit("/");
+    cy.get("#open-drawer-button").click();
+    cy.get("#temporary-drawer").within(() => {
+      cy.get("#Code-item-button").click();
+    });
+    cy.url().should("include", "/code");
+  });
+});
