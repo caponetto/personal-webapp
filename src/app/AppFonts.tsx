b6ba3b1d7ee2ae8c4bbb80d49717117076dacc13
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2022-02-09 17:24:07 -0300

    Improve About and Journey pages

--- src/app/AppFonts.tsx
+++ src/app/AppFonts.tsx
@@ -1,7 +1,13 @@
+import "@fontsource/nunito/400.css";
+import "@fontsource/oxygen/400.css";
+import "@fontsource/philosopher/400.css";
 import "@fontsource/quicksand/400.css";
 import "@fontsource/roboto/400.css";
 
 export enum AppFonts {
+  NUNITO = "Nunito",
+  OXYGEN = "Oxygen",
+  PHILOSOPHER = "Philosopher",
   QUICKSAND = "Quicksand",
   ROBOTO = "Roboto",
 }
