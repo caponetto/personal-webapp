134c2915f9c7acc7f3897c76482ff0dca39169bb
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2022-04-02 08:34:28 -0300

    Add context path to load i18n locales

--- src/i18n/index.tsx
+++ src/i18n/index.tsx
@@ -3,6 +3,8 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
+const contextPath = process.env["WEBPACK_REPLACE__contextPath"];
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
@@ -16,7 +18,7 @@ i18n
     },
     debug: false,
     backend: {
-      loadPath: "/static/locales/{{lng}}/{{ns}}.json",
+      loadPath: `${contextPath}/static/locales/{{lng}}/{{ns}}.json`,
     },
   });
 
