810f0c8d319a2ab23b1e75cdc0ad89f1090579ba
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2022-06-16 06:59:33 -0700

    Simplify tests with custom commands (#58)

--- cypress/integration/TextPage.test.ts
+++ cypress/integration/TextPage.test.ts
@@ -1,33 +1,33 @@
 describe("TextPage", () => {
   beforeEach(() => {
-    cy.viewport(1000, 800);
+    cy.setScreen("large");
   });
 
   it("should navigate to /text when clicking in the Text list item", () => {
     cy.visit("/");
-    cy.get('[data-testid="permanent-drawer"]').within(() => {
-      cy.get('[data-testid="text-item-button"]').click();
+    cy.getDrawer("permanent").within(() => {
+      cy.getDrawerItem("text").click();
     });
-    cy.url().should("include", "/text");
+    cy.assertLocation("/text");
   });
 
   it("should navigate to /text when clicking in the Text list item (small screen)", () => {
-    cy.viewport(400, 600);
+    cy.setScreen("small");
     cy.visit("/about");
-    cy.get('[data-testid="open-drawer-button"]').click();
-    cy.get('[data-testid="temporary-drawer"]').within(() => {
-      cy.get('[data-testid="text-item-button"]').click();
-      cy.url().should("include", "/text");
+    cy.openDrawer();
+    cy.getDrawer("temporary").within(() => {
+      cy.getDrawerItem("text").click();
+      cy.assertLocation("/text");
     });
   });
 
   it("should select the Text item when clicking on its item", () => {
     cy.visit("/about");
-    cy.get('[data-testid="permanent-drawer"]').within(() => {
-      cy.get('[data-testid="text-item-button"]').should("not.have.class", "Mui-selected");
-      cy.get('[data-testid="text-item-button"]').click();
-      cy.get('[data-testid="text-item-button"]').should("have.class", "Mui-selected");
-      cy.get('[data-testid="about-item-button"]').should("not.have.class", "Mui-selected");
+    cy.getDrawer("permanent").within(() => {
+      cy.getDrawerItem("text").should("not.have.class", "Mui-selected");
+      cy.getDrawerItem("text").click();
+      cy.getDrawerItem("text").should("have.class", "Mui-selected");
+      cy.getDrawerItem("about").should("not.have.class", "Mui-selected");
     });
   });
 });
