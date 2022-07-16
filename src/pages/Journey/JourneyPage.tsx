56617db305c85ee12c0cfbc2709a181d7b8ed811
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2022-07-16 14:48:16 -0700

    Use outlined icon for Awards section (#71)

--- src/pages/Journey/JourneyPage.tsx
+++ src/pages/Journey/JourneyPage.tsx
@@ -1,4 +1,4 @@
-import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import EmojiEventsOutlinedIcon from "@mui/icons-material/EmojiEventsOutlined";
 import HomeRepairServiceOutlinedIcon from "@mui/icons-material/HomeRepairServiceOutlined";
 import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
 import VerifiedOutlinedIcon from "@mui/icons-material/VerifiedOutlined";
@@ -87,7 +87,7 @@ export default function JourneyPage() {
                 <Grid item xs={12} xl={6}>
                   <JourneyCard
                     title={t("literal:awards")}
-                    icon={<EmojiEventsIcon />}
+                    icon={<EmojiEventsOutlinedIcon />}
                     content={<JourneyList kind="award" items={app.schema.journey.award} />}
                   />
                 </Grid>
