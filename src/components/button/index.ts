46735a6b50c9f249fff01f81d45e4be463c6744c
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2024-05-03 21:16:14 -0300

    Update X in favor of Twitter (#103)

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -2,3 +2,4 @@ export { ExternalLinkButton } from "./ExternalLinkButton";
 export { GitHubButton } from "./GitHubButton";
 export { LinkedInButton } from "./LinkedInButton";
 export { TwitterButton } from "./TwitterButton";
+export { XButton } from "./XButton";
